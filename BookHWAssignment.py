47973a52fe907af03d9c841d4c795ff00122fa7b
Fausto Agullo <97686953+faustoagullo@users.noreply.github.com>
2022-03-16 14:01:57 +0100

    Update BookHWAssignment.py

--- BookHWAssignment.py
+++ BookHWAssignment.py
@@ -1,82 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
-from urllib.request import urlopen
-
-url = "https://www.gutenberg.org/files/1342/1342-0.txt"
-local_name = "prideandprejudice.txt"
-
-url_2 = "https://www.gutenberg.org/files/11/11-0.txt"
-local_name_2 = "aliceinwonderland.txt"
-
-import certifi
-import ssl
-
-def save_locally():
-    """
-    Save the book locally, so we can use it faster and no need to load every time
-    :return: None
-    """
-    with open(local_name, "w") as local_fp:
-        with urlopen(url, context=ssl.create_default_context(cafile=certifi.where())) as fp:
-            for line in fp:
-                line = line.decode('utf-8-sig').replace("\n", "")
-                local_fp.write(line)
-
-    with open(local_name_2, "w") as local_fp:
-        with urlopen(url_2, context=ssl.create_default_context(cafile=certifi.where())) as fp:
-            for line in fp:
-                line = line.decode('utf-8-sig').replace("\n", "")
-                local_fp.write(line)
-
-punctuation = ",;!.?-()"
-
-def get_unique_words():
-    """
-    Get the dictionary of unique words and their frequency
-    :return: dict
-    """
-    unique_words = {}
-    unique_words_2 = {}
-
-    with open(local_name) as fp:
-        for line in fp:
-            # remove punctuation
-            for p in punctuation:
-                line = line.replace(p, " ")
-            line = line.lower()
-            # get the words:
-            for word in line.split():
-                unique_words[word] = unique_words.get(word, 0) + 1
-
-    return unique_words
-
-def get_unique_words_2():
-
-    with open(local_name_2) as fp:
-        for line in fp:
-            for p in punctuation:
-                line = line.replace(p, " ")
-            line = line.lower()
-            for word in line.split():
-                unique_words_2[word] = unique_words_2.get(word, 0) + 1
-
-
-
-save_locally()
-unique_words = len(get_unique_words())
-unique_words_2 = len(get_unique_words_2())
-
-print(f"Pride and Prejudice has:{unique_words} unique words.")
-print(f"Alice in Wonderland has: {unique_words_2} unique words.")
-
-
-# In[26]:
-
-
 from urllib.request import urlopen
 
 url = "https://www.gutenberg.org/files/1342/1342-0.txt"
@@ -192,10 +113,6 @@ print(f"Alice in Wonderland has {unique_words7_2} unique words longer than 7 cha
 
 #was not able to find a way to do the 7 character question since the function is not iterable.
         
-    
-
-
-# In[ ]:
 
 
 
